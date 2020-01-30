31d0dd00b531172759f560c6100d9684f6435dcc
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2020-01-30 13:12:36 +0000

    WIP: add readall and write methods

--- avatar2/plugins/memfile.py
+++ avatar2/plugins/memfile.py
@@ -45,6 +45,49 @@ class MemoryView(io.RawIOBase):
     def tell(self):
         return self.offset
 
+    @property
+    def memory_range(self):
+        return self.avatar.get_memory_range(self.offset)
+
+    def readinto(self, b):
+        memory_range = self.memory_range
+        if memory_range is None:
+            # From io.RawIOBase.readinto:
+            # If the object is in non-blocking mode and no bytes
+            # are available, None is returned.
+            return None
+
+        memory_range_end = memory_range.address + memory_range.size
+
+        # By default, try to read as much as possible; if we are close
+        # to the end of a memory range, read just the remaining memory.
+        size = min(len(b), memory_range_end - self.offset)
+
+        read = self.target.read_memory(self.offset, size, raw=True)
+        b[:size] = read
+        self.offset += size
+        return size
+
+    def write(self, data):
+        memory_range = self.memory_range
+        if memory_range is None:
+            # From io.RawIOBase.write:
+            # None is returned if the raw stream is set not to block
+            # and no single byte could be readily written to it
+            return None
+
+        memory_range_end = memory_range.address + memory_range.size
+
+        size = min(len(data), memory_range_end - self.offset)
+        to_write = data[:size]
+
+        ret = self.target.write_memory(self.offset, size, to_write, raw=True)
+        if ret == False:
+            # write failed; return None.
+            return None
+
+        return size
+
 
 def add_methods(target):
     target.memory = MethodType(MemoryView, target)
